a748b48d27a3bca8a3ab90d65a7af5ad2e99b78b
Koen Punt <koen@koenpunt.nl>
2022-10-30 17:49:54 +0100

    add rubocop and fix violations

--- lib/capistrano-nvm/version.rb
+++ lib/capistrano-nvm/version.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module CapistranoNvm
   VERSION = '0.0.7'
 end
--- lib/capistrano/nvm.rb
+++ lib/capistrano/nvm.rb
@@ -1,1 +1,3 @@
-load File.expand_path("../tasks/nvm.rake", __FILE__)
+# frozen_string_literal: true
+
+load File.expand_path('tasks/nvm.rake', __dir__)
